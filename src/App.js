acf0afe34fba07f3c06f8555739da9110ac6b96c
Zaranasirova <zerifenesirova784@gmail.com>
2024-09-22 17:10:02 +0400

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
 import PageContainer from "./components/PageContainer";
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import logo from "../assets/icons/logo.svg";
+import { IoMdBasket } from "react-icons/io";
+import { MdLightMode } from "react-icons/md";
+import { IoMoon } from "react-icons/io5";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [mode, setMode] = useState(true);
+  const handleModeChange = () => {
+    setMode(!mode);
+    const myElement = document.getElementById("root");
 
-export default Header
+    if (myElement) {
+      if (mode) {
+        myElement.classList.add("active");
+      } else {
+        myElement.classList.remove("active");
+      }
+      console.log(myElement);
+    }
+  };
+
+  return (
+    <header className="header">
+      <div className="logo-image">
+        <h1>Z.E</h1>
+      </div>
+      <div className="user-area">
+        <input type="text" className="search-button" placeholder="axtar:" />
+        <div className="icons">
+          <IoMdBasket className="icon" />
+          {mode ? (
+            <MdLightMode className="icon" onClick={handleModeChange} />
+          ) : (
+            <IoMoon className="icon" onClick={handleModeChange} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
